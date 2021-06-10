7920f027a9d596dcd56dbc4bab5c24261c34b09f
dmitrakovaalina <asupv.alina>
2021-06-10 20:03:32 +0300

    modify lab9

--- skills.js
+++ skills.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const skills = [
     { css: "skill-html", skillName: "HTML", skillLevel: 90 },
     { css: "skill-css", skillName: "CSS", skillLevel: 20 },
@@ -32,34 +31,3 @@ function addSkills() {
 }
 
 addSkills();
-=======
-const obj = [
-    { css: "skill-html", skill_name: "HTML", lvl: 90 },
-    { css: "skill-css", skill_name: "CSS",  lvl: 20 },
-    { css: "skill-js", skill_name: "Java Script", lvl: 40 },
-    { css: "skill-j", skill_name: "Java",  lvl: 70 },
-    { css: "skill-spring", skill_name: "Spring",  lvl: 60 },
-    { css: "skill-sql", skill_name: "SQL",  lvl: 75 }
-];
-
-function add() {
-    let dl = document.createElement('dl');
-    dl.setAttribute('class', 'skills-list');
-    for (let i = 0; i < obj.length; i++) {
-        let dt = document.createElement('dt');
-        dt.textContent = obj[i].skill_name;
-        dt.setAttribute('class', obj[i].css);
-        let dd = document.createElement('dd');
-        dd.setAttribute('class', 'level');
-        let div = document.createElement('div');
-        div.setAttribute('style', `width: ${obj[i].lvl}%;`);
-        div.textContent = obj[i].lvl;
-        dd.appendChild(div);
-        dl.append(dt);
-        dl.append(dd);
-    }
-    document.body.append(dl);
-}
-
-add();
->>>>>>> 830ec49d4fa32f49d1ec26c7493fbae729dad682
